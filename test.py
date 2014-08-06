0acc6fdc5e45bed8a97d4c74e8783fac20eef9fb
c00h00g <895281773@qq.com>
2014-08-06 11:55:48 +0800

    添加quer_url文件

--- test.py
+++ test.py
@@ -1,1 +1,8 @@
-print('Hello world!')
+#encoding:UTF-8
+import urllib.request
+fp = urllib.request.urlopen("http://www.baidu.com")
+mybytes = fp.read()
+mystr = mybytes.decode("utf8")
+
+fp.close()
+print(mystr)
